e1c630a4c6a8419358b7fc23c3503b240a32d440
jure-prnaver <jure.prnaver@agiledrop.com>
2023-07-11 18:55:56 +0200

    add loading and error states

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,15 +12,24 @@ export class AppComponent implements OnInit {
   constructor(private weatherService: WeatherService) {}
 
   weatherData?: List[];
+  isLoading?: boolean;
+  isError?: boolean;
 
   currentDate = moment().format('DD.MM.YYYY [ob] HH:mm');
 
   getTempInCelsius = (temp: number) => temp - 274.15;
 
   ngOnInit(): void {
+    this.isLoading = true;
+    this.isError = false;
+
     this.weatherService.getWeatherData().subscribe({
       next: (response) => {
         this.weatherData = response.list;
+        this.isLoading = false;
+      },
+      error: () => {
+        this.isError = true;
       },
     });
   }
