d83a2166d6bfed1b7b044d5a4df31135a4d3af8a
jure-prnaver <jure.prnaver@agiledrop.com>
2023-07-11 18:36:19 +0200

    fetch and display weather data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+import { List, WeatherData } from './models/weather.model';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'weather-app-nomnio';
+export class AppComponent implements OnInit {
+  constructor(private weatherService: WeatherService) {}
+
+  weatherData?: List[];
+
+  currentDate = moment().format('DD.MM.YYYY [ob] HH:mm');
+
+  getTempInCelsius = (temp: number) => temp - 274.15;
+
+  ngOnInit(): void {
+    this.weatherService.getWeatherData().subscribe({
+      next: (response) => {
+        this.weatherData = response.list;
+      },
+    });
+  }
 }
